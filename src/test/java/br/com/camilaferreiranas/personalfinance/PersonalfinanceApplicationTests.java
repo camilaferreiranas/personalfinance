4d17eb84aa8f8035845017971bf906351f594a3d
Camila Ferreira <camila_fn97@hotmail.com>
2025-07-12 17:49:40 -0300

    feat: commit inicial

---
+++ src/test/java/br/com/camilaferreiranas/personalfinance/PersonalfinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.camilaferreiranas.personalfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalfinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
